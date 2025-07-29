19047a60ad3a4575b5d54628fc493b985ad58104
sbx <vsbreturn@gmail.com>
2025-07-29 19:44:00 -0300

    first commit

---
+++ src/main/java/br/vitorsb/delivery_management_api/DeliveryManagementApiApplication.java
@@ -0,0 +1,13 @@
+package br.vitorsb.delivery_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/vitorsb/delivery_management_api/DeliveryManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.vitorsb.delivery_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
